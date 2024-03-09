06f6749160d11e12f6f13c92edeb3113abb6835c
cameron custer <custer.cameron@gmail.com>
2024-03-09 08:36:42 -0500

    whitespace nit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,11 +26,11 @@ vim.api.nvim_set_keymap('c', 'W', 'w', {noremap = true}) -- :W now writes
 exec = 'cat input && echo "----" && ./%:r.out < input'
 vim.api.nvim_set_keymap('n', '<F5>', -- save, compile, and run
 '<CMD>w!<CR>' ..
-'<CMD>!g++ -g -Wall -std=c++20 %:r.cpp -o %:r.out &&' .. exec .. '<CR>', {noremap = true})
+'<CMD>!g++ -g -Wall -std=c++20 %:r.cpp -o %:r.out && ' .. exec .. '<CR>', {noremap = true})
 compile_flags = '-Wall -Wextra -Wunused -Wpedantic -Wshadow -Wlogical-op -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -Wshift-overflow=2 -Wduplicated-cond -std=c++20 -fstack-protector -D_GLIBCXX_DEBUG -D_GLIBCXX_SANITIZE_VECTOR -D_GLIBCXX_DEBUG_PEDANTIC -D_GLIBCXX_ASSERTIONS -D_FORTIFY_SOURCE=2'
 vim.api.nvim_set_keymap('n', '<F6>', -- save, compile with flags, and run
 '<CMD>w!<CR>' ..
-'<CMD>!g++ ' .. compile_flags .. ' %:r.cpp -o %:r.out &&' .. exec .. '<CR>', {noremap = true})
+'<CMD>!g++ ' .. compile_flags .. ' %:r.cpp -o %:r.out && ' .. exec .. '<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<F7>', -- run in GDB
 '<CMD>term cat input && echo "----" && gdb -q -ex \'set args < input\' %:r.out<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<F8>', '<CMD>term cat > input<CR>', {noremap = true}) -- create input file
